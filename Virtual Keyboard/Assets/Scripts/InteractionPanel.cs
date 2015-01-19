bc2bab64357710e21b9d10621a8fa4a467f5e5ee
Mark Roukema <mroukema@gmail.com>
2015-01-19 03:42:17 -0500

    Enabled keyboard key colliders, disabled user-keyboard collisions
    Added a collisionExclusionList to InteractionPanel script. Any objects in
    this list will not collide with any colliders that are children of the
    gameObject that interaction panel is set on.

--- Virtual Keyboard/Assets/Scripts/InteractionPanel.cs
+++ Virtual Keyboard/Assets/Scripts/InteractionPanel.cs
@@ -3,17 +3,28 @@ using System.Collections;
 
 public class InteractionPanel : MonoBehaviour
 {
+	public Collider[] collisionExclusionsList;
 		// Use this for initialization
-		void Start ()
-		{
-	
+	private BoxCollider[] childColliders;
+	void Start ()
+	{
+		// collisionExclusionsList[collisionExclusionsList.Length] = user.collider;
+		// gameObject.GetComponentsInChildren (BoxCollider);
+		childColliders = gameObject.GetComponentsInChildren<BoxCollider>() as BoxCollider[];
+		foreach( Collider childCollider in childColliders ) {
+			if( childCollider.enabled == true ) {
+				foreach( Collider collisonExcluded in collisionExclusionsList ) {
+					Physics.IgnoreCollision( childCollider, collisonExcluded );
+				}
+			}
 		}
-	
-		// Update is called once per frame
-		void Update ()
-		{
-	
-		}
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
 
 	void onCollisionEnter(Collision collision) {
 		Debug.Log ("Something hit the keyboard");
