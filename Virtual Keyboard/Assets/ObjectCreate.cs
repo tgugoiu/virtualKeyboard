3d821e9b0e06dc65120bab2b0faf9644385225cd
Mark Roukema <mroukema@gmail.com>
2015-02-17 20:30:15 -0500

    Added gesture support and can create an object on circle gesture.

---
+++ Virtual Keyboard/Assets/ObjectCreate.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectCreate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		LeapGestureInit.CircularGestureTriggered += HandleCircularGestureTriggered;
+	}
+
+	void HandleCircularGestureTriggered (object sender, System.EventArgs e)
+	{
+		Debug.Log ("We're creating an object!!");
+		GameObject gestureCreatedObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		gestureCreatedObject.transform.SetParent (gameObject.transform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
